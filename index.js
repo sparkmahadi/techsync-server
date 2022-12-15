334af2c462e25c90504128ba23047d9b5b5af080
sparkmahadi <mahadi.development@gmail.com>
2022-12-15 21:38:48 +0600

    integrated database

--- index.js
+++ index.js
@@ -1,50 +1,69 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const courseCategories = require('./data/category.json')
-const courses = require('./data/courses.json');
-const courseDetails = require('./data/course-details.json');
-const faq = require('./data/FAQ.json');
-const blog = require('./data/blog.json');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gp7ekja.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-app.get('/', (req, res) => {
-    res.send('techsync data is running')
-});
+async function run() {
+    const courseCategories = client.db("techsync").collection("categories");
+    const coursesCollection = client.db("techsync").collection("courses");
+    const courseDetailsCollection = client.db("techsync").collection("courseDetails");
+    const faqsCollection = client.db("techsync").collection("faqs");
+    const blogsCollection = client.db("techsync").collection("blogs");
 
-app.get('/courses', (req, res) => {
-    res.send(courses);
-})
+    app.get('/', (req, res) => {
+        res.send('techsync server is running')
+    });
+    
+    app.get('/courses', async(req, res) => {
+        const query = {};
+        const result = await coursesCollection.find(query).toArray();
+        res.send(result);
+    })
+    
+    app.get('/course-categories', async(req, res) => {
+        const query = {};
+        const result = await courseCategories.find(query).toArray();
+        res.send(result);
+    })
+    
+    app.get('/course-details/:id', async(req, res) => {
+        const id = parseInt(req.params.id);
+        const query = {id: id};
+        const result = await courseDetailsCollection.findOne(query);
+        res.send(result);
+    })
+    
+    app.get('/course-category/:level', async(req, res) => {
+        const level = req.params.level;
+        const query = {level: level};
+        const result = await coursesCollection.find(query).toArray();
+        res.send(result);
+    })
+    
+    app.get('/faq', async(req, res) => {
+        const query = {};
+        const result = await faqsCollection.find(query).toArray();
+        res.send(result);
+    })
+    
+    app.get('/blog', async(req, res) => {
+        const query = {};
+        const result = await blogsCollection.find(query).toArray();
+        res.send(result);
+    })
 
-app.get('/course-categories', (req, res) => {
-    res.send(courseCategories);
-})
+}
 
-app.get('/course-details/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const selectedCourse = courseDetails.find(c => c.id === id);
-    console.log(selectedCourse);
-    res.send(selectedCourse);
-})
-
-app.get('/course-category/:level', (req, res) => {
-    const level = req.params.level;
-    const selectedCourses = courses.filter(course => course.level == level);
-    console.log(selectedCourses);
-    res.send(selectedCourses);
-})
-
-app.get('/faq', (req, res) => {
-    res.send(faq);
-})
-
-app.get('/blog', (req, res) => {
-    res.send(blog);
-})
+run().catch(err => console.log(err))
 
 app.listen(port, () => {
     console.log(`techsync data is running on port ${port}`);
